05a4e5dc82527d654e6e51023d70d480cdbd499d
Ilya K <95108691+Hagonata@users.noreply.github.com>
2023-12-08 14:43:31 +0100

    Update utils.py
    torch._six to torch
    
    So you can use it with all torch versions.

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ from pathlib import Path
 import subprocess
 import torch
 import torch.distributed as dist
-from torch._six import inf
+from torch import inf
 import random
 
 from tensorboardX import SummaryWriter
